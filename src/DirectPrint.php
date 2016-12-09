5eb290d516096c685c4703229e88dbf983dfef61
Andreas Lemke <andreas.lemke@tu-braunschweig.de>
2016-12-09 01:56:41 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/DirectPrint.php
+++ src/DirectPrint.php
@@ -7,58 +7,52 @@ namespace Neysi\DirectPrint;
 
 class DirectPrint
 {
+    public static function getPrinters()
+    {
+        exec('lpstat -a | awk \'{print $1}\'', $out);
 
-
-
-  public static function getPrinters()
-  {
-    exec('lpstat -a | awk \'{print $1}\'',$out);
-
-    return is_array($out) ? $out : [];
-  }
-
-  public function cancelAllJobs()
-  {
-      exec('cancel -a');
-  }
-
-  public function cancelJob($jobID)
-  {
-      exec('cancel -a '.$jobID);
-  }
-
-  public function activeJobs()
-  {
-      exec('lpstat -o',$out);
-      return $out;
-  }
-
-  public static function printFile($fileName, $printerName='default')
-  {
-     if($printerName=='default'){
-       $printerName = self::getDefaultPrinter();
-     }
-    exec('lp -d '.$printerName.' '. $fileName,$out);
-
-    //Get job id
-    preg_match_all("!-([\d]+) \((.*)\)!",$out[0],$arr);
-
-    if (isset($arr[1])){
-          $id = $printerName .'-'. $arr[1][0];
-    }else{
-          $id = null;
+        return is_array($out) ? $out : array();
     }
 
-    return $id;
+    public function cancelAllJobs()
+    {
+        exec('cancel -a');
+    }
 
-  }
+    public function cancelJob($jobID)
+    {
+        exec('cancel -a '.$jobID);
+    }
 
-  public static function getDefaultPrinter()
-  {
-      exec('lpstat -d', $out);
+    public function activeJobs()
+    {
+        exec('lpstat -o', $out);
+        return $out;
+    }
 
-      return preg_replace('/(.+): /', '', $out[0]) ;
+    public static function printFile($fileName, $printerName='default')
+    {
+        if ($printerName=='default') {
+            $printerName = self::getDefaultPrinter();
+        }
+        exec('lp -d '.$printerName.' '. $fileName, $out);
 
-  }
+    //Get job id
+    preg_match_all("!-([\d]+) \((.*)\)!", $out[0], $arr);
 
+        if (isset($arr[1])) {
+            $id = $printerName .'-'. $arr[1][0];
+        } else {
+            $id = null;
+        }
+
+        return $id;
+    }
+
+    public static function getDefaultPrinter()
+    {
+        exec('lpstat -d', $out);
+
+        return preg_replace('/(.+): /', '', $out[0]) ;
+    }
 }
--- tests/DirectPrintTests.php
+++ tests/DirectPrintTests.php
@@ -4,18 +4,16 @@ use Neysi\DirectPrint\DirectPrint;
 
 class DirectPrintTest extends PHPUnit_Framework_TestCase
 {
+    public function testNothing()
+    {
+        $this->assertTrue(true);
+    }
 
-  public function testNothing()
-  {
-    $this->assertTrue(true);
-  }
 
+    public function testGetPrinters()
+    {
+        $printers = DirectPrint::getPrinters();
 
-  public function testGetPrinters()
-  {
-    $printers = DirectPrint::getPrinters();
-
-    return ( $printers ) > 0;
-  }
-
+        return ($printers) > 0;
+    }
 }
